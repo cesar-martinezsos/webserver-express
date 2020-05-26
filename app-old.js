615447cf8e8aade3952c90a8c8511b582b4b3964
Cesar Martinez <cesar.martinez@sos-soft.com>
2020-05-25 22:34:34 -0500

    Primer Commit

---
+++ app-old.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    let salida = {
+        nombre: 'cesar',
+        edad: 31,
+        url: res.url
+    }
+
+    res.write(JSON.stringify(salida));
+    res.end();
+
+}).listen(8080);
+
+console.log('Escuchando el puerto 8080');
